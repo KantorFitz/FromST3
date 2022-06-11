df9b747b89e7c633d9d38a40867817898c15cab8
Krzysztof Jakubowski <fakedperson@o2.pl>
2022-06-11 14:41:05 +0200

    1

---
+++ ExamampleApp2Approach/Entities/Hardware.cs
@@ -0,0 +1,14 @@
+﻿namespace ExamampleApp2Approach.Entities
+{
+	public class Hardware
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Year { get; set; }
+		public string NumberOfItems { get; set; }
+		public decimal Price { get; set; }
+		
+		public int CompanyId { get; set; }
+	}
+}
---
+++ ExamampleApp2Approach/Models/HardwareModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ExamampleApp2Approach.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ExampleApp.Model
+{
+	public class HardwareModel
+	{
+		public HardwareModel()
+		{
+			var result = new List<Company>(); //change on serv class
+			Companies = new SelectList(result, "Id", "Name");
+		}
+
+		public SelectList Companies { get; set; }
+		public Hardware Hardware { get; set; }
+	}
+}
