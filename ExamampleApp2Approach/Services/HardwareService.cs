eb1958c344f94bf1d79a2523fb616d409cb82ac9
Krzysztof Jakubowski <fakedperson@o2.pl>
2022-06-25 13:52:58 +0200

    next

--- ExamampleApp2Approach/Services/HardwareService.cs
+++ ExamampleApp2Approach/Services/HardwareService.cs
@@ -1,7 +1,29 @@
-﻿namespace ExamampleApp2Approach.Service
+﻿using System.Collections.Generic;
+using ExamampleApp2Approach.Entities;
+using ExampleApp.Model;
+
+namespace ExamampleApp2Approach.Service
 {
 	public class HardwareService
 	{
+		public void Add(Hardware hardware)
+		{
+		}
+
+		public List<Hardware> GetHardware()
+		{
+			var result = new List<Hardware>();
+
+			return result;
+		}
+
+		public List<Company> GetCompanies()
+		{
+			var result = new List<Company>();
+
+			return result;
+		}
+		
 		
 	}
 }
